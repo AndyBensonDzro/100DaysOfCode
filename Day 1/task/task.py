bd887800b1986247b290b5709759984cd4d9e946
Andy Dzro <adzro01@student.bbk.ac.uk>
2026-05-18 20:49:50 +0100

    Initial commit - project setup

---
+++ Day 1/task/task.py
@@ -0,0 +1,8 @@
+#Print out welcome message for the user.
+print("Welcome to the Band Name Generator.")
+#Ask user to input the name of the city the grew up in and store it in a variable called 'city'.
+city = input("What's the name of the city you grew up in?\n")
+#Ask user to input the name of their pet and store it in a variable called 'pet_name'
+pet_name = input("What's your pet's name?\n")
+#Print out band name by concatenating city and pet name given by the user
+print("Your band name could be " + city + " " + pet_name)
